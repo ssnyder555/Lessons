66f891f08bb91a16ca6136f1bfdbc2e1b783daa0
Stephen Snyder <ssnyder555@live.com>
2018-10-15 11:04:13 -0500

    mongoose connected

--- blog-lesson/server.js
+++ blog-lesson/server.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app  = express();
 
+ require('/db/db');
+
+app.get('/', (req, res) => {
+  res.render('index.ejs');
+})
+
 app.listen(3000, () => {
   console.log('listening');
 })
