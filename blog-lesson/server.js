1734561ad970d72ed66e140a64b9f929e0921113
Stephen Snyder <ssnyder555@live.com>
2018-10-15 11:56:50 -0500

    added show route

--- blog-lesson/server.js
+++ blog-lesson/server.js
@@ -17,6 +17,15 @@ app.get('/', (req, res) => {
   res.render('index.ejs');
 });
 
+router.get('/:id', (req, res) => {
+  Author.findById(req.params.id, (err, authorFound) => {
+
+    res.render('/authors/shows.ejs', {
+      author: authorFound
+    })
+  });
+})
+
 
 app.listen(3000, () => {
   console.log('listening on port 3000');
